6122db5afa8415a13b5cd364344d924161baac6e
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-22 13:08:15 +0530

    sidebar implementation completed

--- src/pages/CreateStudent.tsx
+++ src/pages/CreateStudent.tsx
@@ -1,8 +1,35 @@
-import React from "react";
-import TopBar from "@/components/TopBar";
 import Sidebar from "@/components/SideBar";
-const CreateStudent = () => {
-  return <div>create student page guys</div>;
+import TopBar from "@/components/TopBar";
+import React, { useState } from "react";
+import "tailwindcss/tailwind.css";
+interface SidebarProps {
+  isOpen: boolean;
+}
+const CreateStudent: React.FC<SidebarProps> = ({ isOpen }) => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  //function to handle user button click
+
+  const handleSidebarToggle = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+  return (
+    <div>
+      <TopBar />
+      <div className="flex">
+        <div
+          className={`md:block md:w-1/6 bg-white h-screen shadow-lg ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <Sidebar isOpen={isSidebarOpen} />
+        </div>
+        <div className="md:block md:w-5/6 bg-gray-200 h-screen w-full ">
+          CreateStudent
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateStudent;
--- src/pages/StudentsList.tsx
+++ src/pages/StudentsList.tsx
@@ -1,11 +1,33 @@
+import Sidebar from "@/components/SideBar";
 import TopBar from "@/components/TopBar";
-import React from "react";
+import React, { useState } from "react";
 import "tailwindcss/tailwind.css";
-const StudentsList = () => {
+interface SidebarProps {
+  isOpen: boolean;
+}
+const StudentsList: React.FC<SidebarProps> = ({ isOpen }) => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  //function to handle user button click
+
+  const handleSidebarToggle = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
   return (
     <div>
       <TopBar />
-      StudentsList
+      <div className="flex">
+        <div
+          className={`md:block md:w-1/6 bg-white h-screen shadow-lg ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <Sidebar isOpen={isSidebarOpen} />
+        </div>
+        <div className="md:block md:w-5/6 bg-gray-200 h-screen w-full ">
+          STudentsList
+        </div>
+      </div>
     </div>
   );
 };
