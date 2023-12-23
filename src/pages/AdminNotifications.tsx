ed81d80413ec824400d04c3d54b640276dede685
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-23 21:05:13 +0530

    admin notification page updated

--- src/pages/AdminNotifications.tsx
+++ src/pages/AdminNotifications.tsx
@@ -34,10 +34,12 @@ const StudentsList: React.FC = () => {
   const notifs = NotifData;
   const [isSidebarOpen, setSidebarOpen] = useState(false); //to check whether sidebar is open in responsive view
   const [isModalOpen, setModalOpen] = useState(false); //to check whether the student details modal is open
-  const [selectedStudent, setSelectedStudent] = useState<any>(null);
   const [isBlurry, setBlurry] = useState(false); //to blur background when modal is open
+  const [selectedStudent, setSelectedStudent] = useState<any>(null);
   const [studentsList, setStudentsList] = useState<Student[]>(StudentData); //to map student list
+
   const [notifsList, setNotifsList] = useState<Notif[]>(NotifData);
+  const [selectedNotif, setSelectedNotif] = useState<any>(null);
 
   const handleSidebarToggle = () => {
     //handles sidebar open or not in mobile view
@@ -47,6 +49,11 @@ const StudentsList: React.FC = () => {
   const handleStudentClick = (student: any) => {
     //to select particular student to show details of in student details modal
     setSelectedStudent(student);
+    setModalOpen(true);
+  };
+  const handleNotifClick = (notif: any) => {
+    //to select particular student to show details of in student details modal
+    setSelectedNotif(notif);
     setModalOpen(true);
   };
 
@@ -61,13 +68,15 @@ const StudentsList: React.FC = () => {
 
   return (
     <div>
-      {isModalOpen && selectedStudent && (
+      {isModalOpen && selectedNotif && (
         //modal for student details
         <div className="md:w-1/6 md:block shadow-lg">
           <div className="fixed inset-0 flex items-center justify-center z-50">
             <div className="bg-white md:p-8 p-4  rounded-lg md:w-1/2 w-2/3 shadow-md">
               <div className="flex justify-between">
-                <h2 className="text-xl font-semibold">Notification </h2>
+                <h2 className="text-xl font-semibold">
+                  Notification {selectedNotif.nid}
+                </h2>
                 <button
                   onClick={() => setModalOpen(false)}
                   className="hover:bg-gray-200 px-1 py-1 rounded-lg"
@@ -75,7 +84,7 @@ const StudentsList: React.FC = () => {
                   <IoClose size={24} />
                 </button>
               </div>
-
+              <p className="mt-4">{selectedNotif.content}</p>
               {/* ... other details */}
             </div>
           </div>
@@ -109,10 +118,7 @@ const StudentsList: React.FC = () => {
             <div className="flex bg-gray-100">
               <div className={`m-auto w-full ${isBlurry ? "blur" : ""}`}>
                 {notifsList.map((notif) => (
-                  <div
-                    key={notif.nid}
-                    onClick={() => handleStudentClick(notif)}
-                  >
+                  <div key={notif.nid} onClick={() => handleNotifClick(notif)}>
                     <form>
                       <div className="mt-5 bg-white rounded-md shadow-lg mx-4 text-center items-center">
                         <div className="px-5 pb-5 w-full">
