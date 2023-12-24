061971ce880f3e9a90bd7c15ca6839f73164f3b1
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-24 21:48:33 +0530

    admin rooms updated

--- src/pages/AdminRooms.tsx
+++ src/pages/AdminRooms.tsx
@@ -26,8 +26,8 @@ const AdminRooms: React.FC = () => {
           <Sidebar isOpen={isSidebarOpen} />
         </div>
         <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full flex justify-center items-center">
-          <div className="flex justify-around  bg-red-100 mx-5 mt-5 space-x-5 h-5/6">
-            <div className="bg-green-200 p-4 w-1/3">
+          <div className="flex justify-around   mx-5 mt-5 space-x-5 h-5/6">
+            <div className="bg-white p-4 w-1/3 rounded-md shadow-md ">
               <h1 className="flex justify-center font-semibold text-2xl">
                 Occupied Rooms
               </h1>
@@ -39,14 +39,21 @@ const AdminRooms: React.FC = () => {
                 smth2
               </div>
             </div>
-            <div className="bg-yellow-200 p-4 w-1/3 right-0">
+            <div className="bg-blue-200 p-4 w-1/3 right-0 rounded-md shadow-md">
               <h1 className="flex justify-center font-semibold text-2xl">
                 Unoccupied Rooms
               </h1>
+              <div className="mt-2 flex items-center justify-center border-b  p-3 border-black"></div>
+              <div className="flex items-center justify-center border-b  p-3 border-black">
+                lala
+              </div>
+              <div className="flex items-center justify-center border-b  p-3 border-black">
+                lala2
+              </div>
             </div>
           </div>
-          <div className="mt-2 flex justify-center">
-            <button className="px-4 py-2 rounded-md bg-black text-white flex hover:bg-gray-800">
+          <div className="mt-5 flex justify-center">
+            <button className="px-4 py-2 rounded-md bg-black text-white flex hover:bg-gray-800 mt-5">
               <FaPlus size={18} className="mt-1 mr-2" /> Add Rooms
             </button>
           </div>
