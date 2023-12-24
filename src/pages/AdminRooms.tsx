24483343e799149feb6ddb021266147db22df9b8
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-24 21:27:25 +0530

    admin rooms basic structure implemented

--- src/pages/AdminRooms.tsx
+++ src/pages/AdminRooms.tsx
@@ -2,6 +2,8 @@ import Sidebar from "@/components/SideBar";
 import TopBar from "@/components/TopBar";
 import React, { useState } from "react";
 import "tailwindcss/tailwind.css";
+import { FaPlus } from "react-icons/fa";
+
 interface SidebarProps {
   isOpen: boolean;
 }
@@ -23,8 +25,31 @@ const AdminRooms: React.FC = () => {
         >
           <Sidebar isOpen={isSidebarOpen} />
         </div>
-        <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full ">
-          AdminRooms
+        <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full flex justify-center items-center">
+          <div className="flex justify-around  bg-red-100 mx-5 mt-5 space-x-5 h-5/6">
+            <div className="bg-green-200 p-4 w-1/3">
+              <h1 className="flex justify-center font-semibold text-2xl">
+                Occupied Rooms
+              </h1>
+              <div className="mt-2 flex items-center justify-center border-b  p-3 border-black"></div>
+              <div className="flex items-center justify-center border-b  p-3 border-black">
+                smth
+              </div>
+              <div className="flex items-center justify-center border-b  p-3 border-black">
+                smth2
+              </div>
+            </div>
+            <div className="bg-yellow-200 p-4 w-1/3 right-0">
+              <h1 className="flex justify-center font-semibold text-2xl">
+                Unoccupied Rooms
+              </h1>
+            </div>
+          </div>
+          <div className="mt-2 flex justify-center">
+            <button className="px-4 py-2 rounded-md bg-black text-white flex hover:bg-gray-800">
+              <FaPlus size={18} className="mt-1 mr-2" /> Add Rooms
+            </button>
+          </div>
         </div>
       </div>
     </div>
