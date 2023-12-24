a6b364cb2eb1ccb704bd568d4e788b8f6d09b272
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-24 15:02:52 +0530

    test page updated

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Sidebar from "@/components/SideBar";
+import TopBar from "@/components/TopBar";
+import React, { useState } from "react";
+import "tailwindcss/tailwind.css";
+interface SidebarProps {
+  isOpen: boolean;
+}
+const TestPage: React.FC = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  //function to handle user button click
 
-const TestPage = () => {
-  return <div>TestPage</div>;
+  const handleSidebarToggle = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+  return (
+    <div>
+      <TopBar onSidebarToggle={handleSidebarToggle} />
+      <div className="flex">
+        <div
+          className={`md:block md:w-1/6 bg-white h-screen shadow-lg ${
+            isSidebarOpen ? "block" : "hidden"
+          }`}
+        >
+          <Sidebar isOpen={isSidebarOpen} />
+        </div>
+        <div className="md:block md:w-5/6 bg-gray-200 h-screen w-full ">
+          Mae changes here
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TestPage;
