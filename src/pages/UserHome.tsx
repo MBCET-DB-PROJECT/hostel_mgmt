94a1a56682c448ba416c367ec795e4c0c1f8747f
Adlu Rahman <adlurahman050@gmail.com>
2023-12-26 16:59:08 +0530

    Image Correction

--- src/pages/UserHome.tsx
+++ src/pages/UserHome.tsx
@@ -20,52 +20,53 @@ const UserHome: React.FC = () => {
       <TopBar onSidebarToggle={handleSidebarToggle} />
       <div className="flex">
         <div
-          className={`md:block md:w-1/6 bg-white h-screen shadow-lg ${
-            isSidebarOpen ? "block" : "hidden"
-          }`}
+          className={`md:block md:w-1/6 bg-white h-screen shadow-lg ${isSidebarOpen ? "block" : "hidden"
+            }`}
         >
           <UserSidebar isOpen={isSidebarOpen} />
         </div>
-        <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full ">
-        <div className='flex bg-slate-100 min-h-screen min-w-full'>
-        <div className='flex flex-col w-full max-h-screen'>
-          <div className='flex md:flex-row flex-col max-w-screen h-1/2'>
-            <div className='flex w-1/2 items-center justify-center'>
-              <div className='flex flex-row w-2/3 h-2/3 bg-gradient-to-b from-blue-500 to-slate-300 align-center items-center rounded-2xl'>
-                <div className='flex w-1/2 h-2/3 items-center justify-center'>
-                  <img src='/Adlu.jpg' className='w-64 h-64 transition-transform transform hover:scale-105 shadow-lg rounded-2xl' alt='Your Image' />
+        <div className="md:flex md:w-5/6 bg-slate-200 h-screen w-full">
+          <div className='flex bg-slate-100 min-h-screen min-w-full'>
+            <div className='flex flex-col w-full max-h-screen'>
+              <div className='flex md:flex-row flex-col max-w-screen min-w-full h-1/2 '>
+                <div className='flex md:w-1/2 md:items-center justify-center '>
+                  <div className='flex flex-row w-2/3 md:h-2/3 h-3/4 bg-gradient-to-b from-blue-500 to-slate-300 align-center items-center rounded-2xl'>
+                    <div className='flex w-1/2 h-full items-center justify-center'>
+                      <img src='/Adlu.jpg' className='w-64 h-64 transition-transform transform hover:scale-105 shadow-lg rounded-2xl' alt='Your Image' />
+                    </div>
+                    <div className='flex flex-col text-left w-1/2'>
+                      <div>Name</div>
+                      <div>Room No</div>
+                      <div>Semester</div>
+                    </div>
+                  </div>
                 </div>
-                <div className='flex flex-col text-left '>
-                  <div>Name</div>
-                  <div>Room No</div>
-                  <div>Semester</div>
+                <div className='flex md:flex-row flex-col md:w-1/2 md:items-center justify-center border-4 border-red-500'>
+                  <div className='flex md:flex-row flex-col md:w-2/3 md:h-2/3 h-full bg-gradient-to-b from-blue-500 to-slate-300 align-center items-center rounded-2xl'>
+                    <div className='flex flex-col'>
+                      <div>Name</div>
+                      <div>Room No</div>
+                      <div>Semester</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='flex flex-row max-w-screen h-1/2'>
+                <div className='flex w-full md:w-1/2 items-center justify-center'>
+                  <div className='w-full md:h-2/3 h-full bg-gradient-to-b from-blue-500 to-slate-300 align-center items-center rounded-2xl'>
+                    {/* Content */}
+                  </div>
+                </div>
+                <div className='flex w-full md:w-1/2 items-center justify-center'>
+                  <div className='w-full md:h-2/3 h-full bg-gradient-to-b from-blue-500 to-slate-300 align-center items-center rounded-2xl'>
+                    {/* Content */}
+                  </div>
                 </div>
               </div>
             </div>
-            <div className='flex w-1/2 items-center justify-center'>
-              <div className='w-2/3 h-2/3 bg-gradient-to-b from-blue-500 to-slate-300 align-center items-center rounded-2xl'>
-               <div>
+          </div>
+        </div>
 
-               </div>
-              </div>
-            </div>
-          </div>
-          <div className='flex flex-row max-w-screen h-1/2'>
-            <div className='flex w-1/2 items-center justify-center'>
-              <div className='w-2/3 h-2/3 bg-gradient-to-b from-blue-500 to-slate-300 align-center items-center rounded-2xl'>
-  
-              </div>
-            </div>
-            <div className='flex w-1/2 items-center justify-center'>
-              <div className='w-2/3 h-2/3 bg-gradient-to-b from-blue-500 to-slate-300 align-center items-center rounded-2xl'>
-  
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-  
-        </div>
       </div>
     </div>
   );
@@ -73,4 +74,3 @@ const UserHome: React.FC = () => {
 
 export default UserHome;
 
-  
