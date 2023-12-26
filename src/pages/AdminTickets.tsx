7a61bb6f2f94ab1dd8f01201eb3f8895fbafb484
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-26 12:40:15 +0530

    student count updated in admin ticket page

--- src/pages/AdminTickets.tsx
+++ src/pages/AdminTickets.tsx
@@ -2,22 +2,14 @@ import Sidebar from "@/components/SideBar";
 import TopBar from "@/components/TopBar";
 import React, { useState } from "react";
 import "tailwindcss/tailwind.css";
-import { BsPersonFillAdd } from "react-icons/bs";
+import { FaEdit, FaChevronDown } from "react-icons/fa";
 import Link from "next/link";
-import CreateStudent from "./CreateStudent";
-import { IoClose } from "react-icons/io5";
-import { useEffect } from "react";
-import StudentData from "../data/StudentDetails.json";
-import { MdEditNotifications } from "react-icons/md";
-import NotifData from "../data/Notifications.json";
-import CreateNotifications from "./CreateNotifications";
-import { FaEdit } from "react-icons/fa";
-import { FaChevronDown } from "react-icons/fa";
 import TicketDetails from "./../data/TicketDetails.json";
 
 interface SidebarProps {
   isOpen: boolean;
 }
+
 interface Student {
   id: number;
   name: string;
@@ -30,55 +22,52 @@ interface Student {
 }
 
 const AdminTicket: React.FC = () => {
-  const students = StudentData;
   const [ticketsList, setTicketsList] = useState(TicketDetails); // State to store ticket details
-  const [isSidebarOpen, setSidebarOpen] = useState(false); //to check whether sidebar is open in responsive view
-
-  const [selectedStudent, setSelectedStudent] = useState<any>(null);
-  const [studentsList, setStudentsList] = useState<Student[]>(StudentData); //to map student list
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
   const handleSidebarToggle = () => {
-    //handles sidebar open or not in mobile view
     setSidebarOpen(!isSidebarOpen);
   };
-  const [isDropdownOpen, setDropdownOpen] = useState(false); // State variable for dropdown visibility
 
   const toggleDropdown = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault(); // Prevent the default form submission behavior
-    setDropdownOpen(!isDropdownOpen); // Toggle dropdown visibility
+    event.preventDefault();
+    setDropdownOpen(!isDropdownOpen);
   };
 
   return (
     <div>
-      <div>
-        <TopBar onSidebarToggle={handleSidebarToggle} />
-        <div className="flex">
-          <div
-            className={`md:block md:w-1/6 bg-white h-screen shadow-lg ${
-              isSidebarOpen ? "block" : "hidden"
-            }`}
-          >
-            <Sidebar isOpen={isSidebarOpen} />
+      <TopBar onSidebarToggle={handleSidebarToggle} />
+      <div className="flex">
+        <div
+          className={`md:block md:w-1/6 bg-white h-screen shadow-lg ${
+            isSidebarOpen ? "block" : "hidden"
+          }`}
+        >
+          <Sidebar isOpen={isSidebarOpen} />
+        </div>
+        <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full ">
+          <div className="flex justify-between text-center">
+            <h1 className="mt-6 font-semibold text-3xl flex-1">Tickets</h1>
+            <Link
+              href="/CreateNotifications"
+              className={`mt-6 mr-4 px-2 py-2 bg-gray-400 rounded-lg hover:bg-gray-300 flex ${
+                isSidebarOpen ? "hidden md:block" : ""
+              }`}
+            >
+              <FaEdit size={22} className="mr-1" />
+              Edit Tickets
+            </Link>
           </div>
-          <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full ">
-            <div className="flex justify-between text-center">
-              <h1 className=" mt-6 font-semibold text-3xl flex-1">Tickets</h1>
-              <Link
-                href="/CreateNotifications"
-                className={`mt-6 mr-4 px-2 py-2 bg-gray-400 rounded-lg hover:bg-gray-300 flex ${
-                  isSidebarOpen ? "hidden md:block" : ""
-                }`}
-              >
-                <FaEdit size={22} className="mr-1" />
-                Edit Tickets
-              </Link>
-            </div>
 
-            <div className="flex bg-slate-200">
-              <div className={`m-auto w-full `}>
-                <div>
-                  <form>
-                    {ticketsList.map((ticket) => (
+          <div className="flex bg-slate-200">
+            <div className={`m-auto w-full `}>
+              <div>
+                <form>
+                  {ticketsList.map((ticket) => {
+                    const studentCount = ticket.students.length;
+
+                    return (
                       <div
                         key={ticket.tid}
                         className="mt-5 bg-white rounded-md shadow-lg mx-4 text-center items-center"
@@ -87,7 +76,7 @@ const AdminTicket: React.FC = () => {
                           <div className="flex justify-between py-2">
                             <div>{ticket.name}</div>
                             <div className="flex flex-row space-x-5">
-                              <div>Students raised : 10</div>
+                              <div>Students raised: {studentCount}</div>
                               <button
                                 className="p-1 bg-gray-300 rounded-md hover:bg-gray-200"
                                 onClick={toggleDropdown}
@@ -98,21 +87,20 @@ const AdminTicket: React.FC = () => {
                           </div>
                         </div>
                       </div>
-                    ))}
-                    {isDropdownOpen && (
-                      <div className="mt-3 mx-4 bg-white rounded-md shadow-lg p-2">
-                        {/* Dropdown items go here */}
-                        <div className="mt-2 flex items-center justify-between border-b p-3 border-black">
-                          <h1>thingy 1</h1>
-                          <p>roomno</p>
-                        </div>
-                        <div className="mt-2 flex items-center justify-between border-b p-3 border-black">
-                          thingy 2
-                        </div>
+                    );
+                  })}
+                  {isDropdownOpen && (
+                    <div className="mt-3 mx-4 bg-white rounded-md shadow-lg p-2">
+                      <div className="mt-2 flex items-center justify-between border-b p-3 border-black">
+                        <h1>thingy 1</h1>
+                        <p>roomno</p>
                       </div>
-                    )}
-                  </form>
-                </div>
+                      <div className="mt-2 flex items-center justify-between border-b p-3 border-black">
+                        thingy 2
+                      </div>
+                    </div>
+                  )}
+                </form>
               </div>
             </div>
           </div>
