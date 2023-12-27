6e26d7e495d1ed4d84b19807456eb99074e0299a
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-27 23:00:23 +0530

    user raise ticket implemented

--- src/pages/UserTickets.tsx
+++ src/pages/UserTickets.tsx
@@ -4,18 +4,22 @@ import UserSidebar from "@/components/UserSideBar";
 import TopBar from "@/components/TopBar";
 import { useState } from "react";
 import "tailwindcss/tailwind.css";
+import { BiUpArrowAlt } from "react-icons/bi";
 
 interface SidebarProps {
   isOpen: boolean;
 }
 const UserTickets: React.FC = () => {
   const [tickets, setTickets] = useState(["Kettle", "Soap", "Towel"]);
-
+  const [selectedTicket, setSelectedTicket] = useState<string | null>(null);
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   //function to handle user button click
 
   const handleSidebarToggle = () => {
     setSidebarOpen(!isSidebarOpen);
+  };
+  const handleDropdownChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedTicket(e.target.value);
   };
   return (
     <div>
@@ -28,11 +32,46 @@ const UserTickets: React.FC = () => {
         >
           <UserSidebar isOpen={isSidebarOpen} />
         </div>
-        <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full ">
+        <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full justify-center">
           <h1 className="flex justify-center mt-4 text-3xl font-semibold">
             Raise A Ticket
           </h1>
-          <div className="flex justify-center">drop down here </div>
+          <div className="flex justify-center mt-4">
+            <select
+              onChange={handleDropdownChange}
+              className="p-2 border rounded-md w-1/6 "
+            >
+              <option value="" disabled selected>
+                Select a ticket
+              </option>
+              {tickets.map((ticket, index) => (
+                <option
+                  key={index}
+                  value={ticket}
+                  className={
+                    index !== 0
+                      ? "mt-4 border-b border-2 border-black bg-red-100"
+                      : ""
+                  }
+                >
+                  {ticket}
+                </option>
+              ))}
+            </select>
+          </div>
+          {selectedTicket && (
+            <div className="mt-4 flex justify-center space-x-3 flex-col items-center">
+              <div className="mt-4 bg-gray-300 p-4 w-1/6 rounded-md">
+                You have selected:{" "}
+                <div className="font-semibold flex justify-center mt-2 text-lg">
+                  {selectedTicket}
+                </div>
+              </div>
+              <button className="p-2 w-1/10 bg-black text-white mt-6 rounded-md flex hover:bg-gray-500">
+                Raise <BiUpArrowAlt className="mt-1 ml-1 font-bold" size={20} />
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
