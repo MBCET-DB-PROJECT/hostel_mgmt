d07e52531388120511a61b1235bc4d0210237a8a
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-27 23:38:38 +0530

    user notifications created

--- src/pages/UserNotifications.tsx
+++ src/pages/UserNotifications.tsx
@@ -1,7 +1,37 @@
+import "tailwindcss/tailwind.css";
 import React from "react";
+import UserSidebar from "@/components/UserSideBar";
+import TopBar from "@/components/TopBar";
+import { useState } from "react";
+import "tailwindcss/tailwind.css";
 
-const UserNotifications = () => {
-  return <div>UserNotifications</div>;
+interface SidebarProps {
+  isOpen: boolean;
+}
+const UserNotifications: React.FC = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  //function to handle user button click
+
+  const handleSidebarToggle = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+  return (
+    <div>
+      <TopBar onSidebarToggle={handleSidebarToggle} />
+      <div className="flex">
+        <div
+          className={`md:block md:w-1/6 bg-white h-screen shadow-lg ${
+            isSidebarOpen ? "block" : "hidden"
+          }`}
+        >
+          <UserSidebar isOpen={isSidebarOpen} />
+        </div>
+        <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full ">
+          UserNotifications
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserNotifications;
