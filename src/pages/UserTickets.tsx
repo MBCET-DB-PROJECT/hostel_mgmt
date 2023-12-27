b5d5cab622a4aebd673fc5254e24817c15377196
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-27 23:24:05 +0530

    forms implemented in user tickets

--- src/pages/UserTickets.tsx
+++ src/pages/UserTickets.tsx
@@ -37,48 +37,50 @@ const UserTickets: React.FC = () => {
           <UserSidebar isOpen={isSidebarOpen} />
         </div>
         <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full justify-center">
-          <h1 className="flex justify-center mt-6 text-3xl font-semibold ">
-            Raise A Ticket
-          </h1>
-          <div className="flex justify-center mt-8">
-            <select
-              onChange={handleDropdownChange}
-              className="p-2 border rounded-md md:w-1/4 w-1/2 "
-            >
-              <option value="" disabled selected>
-                Select a ticket
-              </option>
-              {tickets.map((ticket, index) => (
-                <option
-                  key={index}
-                  value={ticket}
-                  className={index !== 0 ? "" : ""}
-                >
-                  {ticket}
-                </option>
-              ))}
-            </select>
-          </div>
-          {selectedTicket && (
-            <div className="mt-4 flex justify-center space-x-3 flex-col items-center">
-              <div className="mt-4 bg-gray-300 p-4 md:w-1/4 w-1/2 rounded-md">
-                You have selected:{" "}
-                <div className="font-semibold flex justify-center mt-2 text-lg">
-                  {selectedTicket}
-                </div>
-              </div>
-              <button
-                onClick={handleCreateNotification}
-                className="p-2 w-1/8 bg-black text-white mt-6 rounded-md flex hover:bg-gray-500"
+          <form>
+            <h1 className="flex justify-center mt-6 text-3xl font-semibold ">
+              Raise A Ticket
+            </h1>
+            <div className="flex justify-center mt-8">
+              <select
+                onChange={handleDropdownChange}
+                className="p-2 border rounded-md md:w-1/4 w-1/2 "
               >
-                Raise{" "}
-                <BiUpArrowAlt className="mt-1 ml-1 font-semibold" size={20} />
-              </button>
-              {showToast && (
-                <Toast message={`Raised Ticket. `} type="success" />
-              )}
+                <option value="" disabled selected>
+                  Select a ticket
+                </option>
+                {tickets.map((ticket, index) => (
+                  <option
+                    key={index}
+                    value={ticket}
+                    className={index !== 0 ? "" : ""}
+                  >
+                    {ticket}
+                  </option>
+                ))}
+              </select>
             </div>
-          )}
+            {selectedTicket && (
+              <div className="mt-4 flex justify-center space-x-3 flex-col items-center">
+                <div className="mt-4 bg-gray-300 p-4 md:w-1/4 w-1/2 rounded-md">
+                  You have selected:{" "}
+                  <div className="font-semibold flex justify-center mt-2 text-lg">
+                    {selectedTicket}
+                  </div>
+                </div>
+                <button
+                  onClick={handleCreateNotification}
+                  className="p-2 w-1/8 bg-black text-white mt-6 rounded-md flex hover:bg-gray-500"
+                >
+                  Raise{" "}
+                  <BiUpArrowAlt className="mt-1 ml-1 font-semibold" size={20} />
+                </button>
+                {showToast && (
+                  <Toast message={`Raised Ticket. `} type="success" />
+                )}
+              </div>
+            )}
+          </form>
         </div>
       </div>
     </div>
