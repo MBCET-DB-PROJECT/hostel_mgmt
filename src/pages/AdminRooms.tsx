65acb65ea1e99c7f40c65ec508076f488af640e0
Diya Sundeep <diyasundeep64@gmail.com>
2023-12-27 20:51:11 +0530

    admin room component made

--- src/pages/AdminRooms.tsx
+++ src/pages/AdminRooms.tsx
@@ -5,6 +5,7 @@ import "tailwindcss/tailwind.css";
 import { FaPlus } from "react-icons/fa";
 import { useEffect } from "react";
 import RoomData from "../data/Rooms.json";
+import AdminRoomsComp from "@/components/AdminRoomsComp";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -38,7 +39,6 @@ const AdminRooms: React.FC = () => {
     setRooms(RoomData as Room[]);
   }, []);
 
-
   const occupiedRooms = rooms.filter((room) => room.status === "occupied");
   const unoccupiedRooms = rooms.filter((room) => room.status === "unoccupied");
 
@@ -58,7 +58,8 @@ const AdminRooms: React.FC = () => {
         </div>
 
         <div className="md:block md:w-5/6 bg-slate-200 h-screen w-full flex justify-center items-center flex-col">
-          <div className="flex justify-around   mx-5 mt-5 space-x-5 h-5/6">
+          <AdminRoomsComp />
+          {/* <div className="flex justify-around   mx-5 mt-5 space-x-5 h-5/6">
             <div className=" bg-blue-200 p-4 md:w-1/3 w-1/2 rounded-md shadow-md ">
               <h1 className="flex justify-center font-semibold text-2xl text-center">
 
@@ -90,7 +91,7 @@ const AdminRooms: React.FC = () => {
                 </div>
               ))}
             </div>
-          </div>
+          </div>*/}
           <div className="mt-5 flex justify-center">
             <button className="px-4 py-2 rounded-md bg-black text-white flex hover:bg-gray-800 mt-5">
               <FaPlus size={18} className="mt-1 mr-2" /> Add Rooms
