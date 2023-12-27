fe3ba45e93c49fdde05e17da424cfc97e2ad6b57
gaxatri <gayathriss0106@gmail.com>
2023-12-27 22:15:07 +0530

    log in

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -39,7 +39,25 @@ export default function Landing() {
 
 
 </div>
+<div className="h-screen">
+  <div className="h-full">
+    
+    <div
+      className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+      <div
+        className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+        <img
+          src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+          className="w-full"
+          alt="Sample image" />
+      </div>
 
+    
+      
+  </div>
+</div>
+
+</div>
 </div>
   )
 }
