8f39ad39769fba971d7688184e17e50e28d4047b
Diya Sundeep <diyasundeep64@gmail.com>
2024-01-11 12:36:04 +0530

    updated landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -181,6 +181,9 @@ export default function Home() {
             <div className="text-blue-600 w-full text-3xl mt-40 flex flex-col space-x-1">
               <div>Your one-stop destination for all</div>
               <div> your hostel management activities.</div>
+              <button className="bg-purple-500 text-white font-semibold text-lg rounded-full w-1/2 mt-5 p-2 hover:bg-blue-600">
+                Get Started!
+              </button>
               <div></div>
             </div>
           </div>
@@ -225,7 +228,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
       <div className="flex justify-between h-screen">
         <div className="w-1/2 flex justify-center bg-gray-100">
           <div className="flex justify-center items-center ">
@@ -354,140 +356,239 @@ export default function Home() {
 
         <LoginComponent />
       </div>
-      <div className="bg-gray-100 flex justify-center text-center items-center ">
-        <div className="flex flex-col  w-full justify-center items-center">
-          <div className="p-2 border-black border-2 rounded-full w-1/4 text-center flex justify-center font-semibold text-xl">
-            <FaStarOfLife className="mt-1 mr-2" /> Meet the Team
+      <section className="bg-gray-50 ">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg  md:mt-0 sm:max-w-md xl:p-0 shadow-lg">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                Get Started (with) RightHere!
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                    placeholder="name@company.com"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="confirm-password"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    type="confirm-password"
+                    name="confirm-password"
+                    id="confirm-password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  />
+                </div>
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="terms"
+                      className="font-light text-gray-500 "
+                    >
+                      I accept the{" "}
+                      <a
+                        className="font-medium text-primary-600 hover:underline "
+                        href="#"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </label>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                >
+                  Create an account
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Already have an account?{" "}
+                  <a
+                    href="#"
+                    className="font-medium text-primary-600 hover:underline "
+                  >
+                    Login here
+                  </a>
+                </p>
+              </form>
+            </div>
           </div>
-          <div className="mt-10 flex justify-between px-10">
-            <div className="max-w-sm w-1/5 bg-white border border-gray-200 rounded-lg shadow ">
-              <a href="#">
-                <img
-                  className="rounded-t-lg"
-                  src="https://subzfresh.com/wp-content/uploads/2022/04/apple_158989157.jpg"
-                  alt=""
-                />
-              </a>
-              <div className="p-5">
-                <a href="#">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-                    Abhishek Raman
-                  </h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-700 flex flex-row  justify-around ">
-                  <a
-                    href="https://github.com/ice-weasel"
-                    className="hover:bg-gray-300 rounded-lg p-2"
-                  >
-                    {" "}
-                    <FaGithub size={26} className="" />
-                  </a>
-                  <a
-                    href="https://github.com/ice-weasel"
-                    className="hover:bg-gray-300 rounded-lg p-2"
-                  >
-                    {" "}
-                    <FaLinkedin size={26} />
-                  </a>
-                </p>
-              </div>
+        </div>
+      </section>
+      <div className="flex justify-center items-center h-screen">
+        <div className="bg-gray-100 ">
+          <div className="flex flex-col  w-full justify-center items-center">
+            <div className="p-2 border-black border-2 rounded-full w-1/4 text-center flex justify-center font-semibold text-xl">
+              <FaStarOfLife className="mt-1 mr-2" /> Meet the Team
             </div>
+            <div className="mt-10 flex justify-between px-10">
+              <div className="max-w-sm w-1/5 bg-white border border-gray-200 rounded-lg shadow ">
+                <a href="#">
+                  <img
+                    className="rounded-t-lg"
+                    src="https://subzfresh.com/wp-content/uploads/2022/04/apple_158989157.jpg"
+                    alt=""
+                  />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                      Abhishek Raman
+                    </h5>
+                  </a>
+                  <p className="mb-3 font-normal text-gray-700 flex flex-row  justify-around ">
+                    <a
+                      href="https://github.com/ice-weasel"
+                      className="hover:bg-gray-300 rounded-lg p-2"
+                    >
+                      {" "}
+                      <FaGithub size={26} className="" />
+                    </a>
+                    <a
+                      href="https://github.com/ice-weasel"
+                      className="hover:bg-gray-300 rounded-lg p-2"
+                    >
+                      {" "}
+                      <FaLinkedin size={26} />
+                    </a>
+                  </p>
+                </div>
+              </div>
 
-            <div className="max-w-sm w-1/5 bg-white border border-gray-200 rounded-lg shadow ">
-              <a href="#">
-                <img
-                  className="rounded-t-lg"
-                  src="https://subzfresh.com/wp-content/uploads/2022/04/apple_158989157.jpg"
-                  alt=""
-                />
-              </a>
-              <div className="p-5">
+              <div className="max-w-sm w-1/5 bg-white border border-gray-200 rounded-lg shadow ">
                 <a href="#">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-                    Gayathri S
-                  </h5>
+                  <img
+                    className="rounded-t-lg"
+                    src="https://subzfresh.com/wp-content/uploads/2022/04/apple_158989157.jpg"
+                    alt=""
+                  />
                 </a>
-                <p className="mb-3 font-normal text-gray-700 flex flex-row  justify-around ">
-                  <a
-                    href="https://github.com/ice-weasel"
-                    className="hover:bg-gray-300 rounded-lg p-2"
-                  >
-                    {" "}
-                    <FaGithub size={26} className="" />
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                      Gayathri S
+                    </h5>
                   </a>
-                  <a
-                    href="https://github.com/ice-weasel"
-                    className="hover:bg-gray-300 rounded-lg p-2"
-                  >
-                    {" "}
-                    <FaLinkedin size={26} />
-                  </a>
-                </p>
+                  <p className="mb-3 font-normal text-gray-700 flex flex-row  justify-around ">
+                    <a
+                      href="https://github.com/ice-weasel"
+                      className="hover:bg-gray-300 rounded-lg p-2"
+                    >
+                      {" "}
+                      <FaGithub size={26} className="" />
+                    </a>
+                    <a
+                      href="https://github.com/ice-weasel"
+                      className="hover:bg-gray-300 rounded-lg p-2"
+                    >
+                      {" "}
+                      <FaLinkedin size={26} />
+                    </a>
+                  </p>
+                </div>
               </div>
-            </div>
 
-            <div className="max-w-sm w-1/5 bg-white border border-gray-200 rounded-lg shadow ">
-              <a href="#">
-                <img
-                  className="rounded-t-lg"
-                  src="https://subzfresh.com/wp-content/uploads/2022/04/apple_158989157.jpg"
-                  alt=""
-                />
-              </a>
-              <div className="p-5">
+              <div className="max-w-sm w-1/5 bg-white border border-gray-200 rounded-lg shadow ">
                 <a href="#">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-                    Adlu Rahman
-                  </h5>
+                  <img
+                    className="rounded-t-lg"
+                    src="https://subzfresh.com/wp-content/uploads/2022/04/apple_158989157.jpg"
+                    alt=""
+                  />
                 </a>
-                <p className="mb-3 font-normal text-gray-700 flex flex-row  justify-around ">
-                  <a
-                    href="https://github.com/ice-weasel"
-                    className="hover:bg-gray-300 rounded-lg p-2"
-                  >
-                    {" "}
-                    <FaGithub size={26} className="" />
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                      Adlu Rahman
+                    </h5>
                   </a>
-                  <a
-                    href="https://github.com/ice-weasel"
-                    className="hover:bg-gray-300 rounded-lg p-2"
-                  >
-                    {" "}
-                    <FaLinkedin size={26} />
-                  </a>
-                </p>
+                  <p className="mb-3 font-normal text-gray-700 flex flex-row  justify-around ">
+                    <a
+                      href="https://github.com/ice-weasel"
+                      className="hover:bg-gray-300 rounded-lg p-2"
+                    >
+                      {" "}
+                      <FaGithub size={26} className="" />
+                    </a>
+                    <a
+                      href="https://github.com/ice-weasel"
+                      className="hover:bg-gray-300 rounded-lg p-2"
+                    >
+                      {" "}
+                      <FaLinkedin size={26} />
+                    </a>
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="max-w-sm w-1/5 bg-white border border-gray-200 rounded-lg shadow ">
-              <a href="#">
-                <img
-                  className="rounded-t-lg"
-                  src="https://subzfresh.com/wp-content/uploads/2022/04/apple_158989157.jpg"
-                  alt=""
-                />
-              </a>
-              <div className="p-5">
+              <div className="max-w-sm w-1/5 bg-white border border-gray-200 rounded-lg shadow ">
                 <a href="#">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-                    Diya Sundeep
-                  </h5>
+                  <img
+                    className="rounded-t-lg"
+                    src="https://subzfresh.com/wp-content/uploads/2022/04/apple_158989157.jpg"
+                    alt=""
+                  />
                 </a>
-                <p className="mb-3 font-normal text-gray-700 flex flex-row  justify-around ">
-                  <a
-                    href="https://github.com/ice-weasel"
-                    className="hover:bg-gray-300 rounded-lg p-2"
-                  >
-                    {" "}
-                    <FaGithub size={26} className="" />
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                      Diya Sundeep
+                    </h5>
                   </a>
-                  <a
-                    href="https://github.com/ice-weasel"
-                    className="hover:bg-gray-300 rounded-lg p-2"
-                  >
-                    {" "}
-                    <FaLinkedin size={26} />
-                  </a>
-                </p>
+                  <p className="mb-3 font-normal text-gray-700 flex flex-row  justify-around ">
+                    <a
+                      href="https://github.com/ice-weasel"
+                      className="hover:bg-gray-300 rounded-lg p-2"
+                    >
+                      {" "}
+                      <FaGithub size={26} className="" />
+                    </a>
+                    <a
+                      href="https://github.com/ice-weasel"
+                      className="hover:bg-gray-300 rounded-lg p-2"
+                    >
+                      {" "}
+                      <FaLinkedin size={26} />
+                    </a>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
