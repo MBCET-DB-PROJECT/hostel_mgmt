51ce266c38cc414b9ca974935043b793c914a0e7
Abhishek <abhishek.raman018@gmai.com>
2024-01-12 05:24:03 +0530

    removed dupliate imports

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,7 +5,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { useEffect } from "react";
 import "./../app/globals.css";
 import { Canvas } from "@react-three/fiber";
 import Blob from "../components/Blob";
